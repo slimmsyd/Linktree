6c259825e221c26694e5201e1f6af7e65ace804d
Sydney Sanders <syd@Sydneys-MacBook-Air.local>
2022-06-25 01:43:53 -0400

    no discord

--- src/App.js
+++ src/App.js
@@ -7,21 +7,27 @@ function App() {
 
       <li className = "bigBox"></li>
       <li>Sydney Sanders</li>
+
       <div className = "App gap">
+
           <div className = "box">
           <a href = 'https://www.sydneysanders.dev/'>Website</a>
               </div>
+
           <div className = "box">
           <a href = 'https://twitter.com/slimmsyd'>Twitter</a>
               </div>
+
           <div className = "box">
           <a href = '#'>Instagram</a>
               </div>
+
           <div className = "box">
           <a href = 'https://www.youtube.com/channel/UCC8yLhVL7lThf295DtD4rag'>Youtube</a>
               </div>
+              
           <div className = "box">
-          <a href = 'https://discord.gg/n7edy28k'>Discord</a>
+          <a href = ''>Discord</a>
               </div>
       </div>
     </div>
